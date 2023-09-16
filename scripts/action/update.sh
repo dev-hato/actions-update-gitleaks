a7e1ca5aea724140bd703faafacecd66d127b14e
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-09-16 10:20:29 +0900

    super-linterのorganization名修正・アップデート (#1236)
    * super-linterのorganization名修正
    
    * ghcr経由にする
    
    * super-linterアップデート
    
    * gitleaksをアップデートしてあげたよ！ (#1238)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/action/update.sh
+++ scripts/action/update.sh
@@ -3,12 +3,12 @@
 version=""
 
 while IFS= read -r -d '' f; do
-  v="$(yq '.jobs.*.steps[].uses | select(. == "github/super-linter*")' "${f}" | sed -e 's/.*@//g')"
+  v="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*")' "${f}" | sed -e 's/.*@//g')"
   if [ -n "${v}" ]; then
     version="${v}"
     break
   fi
 done < <(find .github/workflows -type f -print0)
 
-curl "https://raw.githubusercontent.com/github/super-linter/${version}/TEMPLATES/.gitleaks.toml" >.gitleaks.toml
-yq -i ".repos[].rev = \"$(docker run --entrypoint gitleaks "github/super-linter:slim-${version}" version | sed -e 's/\r//g')\"" .pre-commit-config.yaml
+curl "https://raw.githubusercontent.com/super-linter/super-linter/${version}/TEMPLATES/.gitleaks.toml" >.gitleaks.toml
+yq -i ".repos[].rev = \"$(docker run --entrypoint gitleaks "ghcr.io/super-linter/super-linter:slim-${version}" version | sed -e 's/\r//g')\"" .pre-commit-config.yaml
