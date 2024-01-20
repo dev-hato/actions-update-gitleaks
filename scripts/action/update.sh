9b51014fa1f296ac6298ee0a1bc824d8b24c312e
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-01-20 20:40:51 +0900

    hash形式に対応

--- scripts/action/update.sh
+++ scripts/action/update.sh
@@ -4,11 +4,16 @@ version=""
 
 while IFS= read -r -d '' f; do
   v="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*")' "${f}" | sed -e 's/.*@//g')"
+  v="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*") | . + " # " + line_comment' "${f}" | sed -e 's/.*@//g')"
   if [ -n "${v}" ]; then
+    if [[ "${v}" == *" # v"* ]]; then
+        v="${v#* # }"
+    fi
     version="${v}"
     break
   fi
 done < <(find .github/workflows -type f -print0)
 
+echo "super-linter version = ${version}"
 curl "https://raw.githubusercontent.com/super-linter/super-linter/${version}/TEMPLATES/.gitleaks.toml" >.gitleaks.toml
 yq -i ".repos[].rev = \"$(docker run --entrypoint gitleaks "ghcr.io/super-linter/super-linter:slim-${version}" version | sed -e 's/\r//g')\"" .pre-commit-config.yaml
