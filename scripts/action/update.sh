9eddd1b56b9efd91de0daa81a618f30407d59768
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-01-20 20:33:19 +0900

    commentをつける

--- scripts/action/update.sh
+++ scripts/action/update.sh
@@ -3,7 +3,7 @@
 version=""
 
 while IFS= read -r -d '' f; do
-  v="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*")' "${f}" | sed -e 's/.*@//g')"
+  v="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*") | . + " # " + line_comment' "${f}" | sed -e 's/.*@//g')"
   if [ -n "${v}" ]; then
     if [[ "${v}" == *" # v"* ]]; then
         v="${v#* # }"
