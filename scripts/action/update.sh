3ffc7bbe6707ff4ab388c833ca62653dd8170cae
Goryudyuma(Kei.Matsumoto) <6211370+Goryudyuma@users.noreply.github.com>
2024-01-20 23:05:29 +0900

    hash形式に対応 (#1325)
    * hash形式に対応
    
    * test
    
    * Revert "test"
    
    This reverts commit 4e689e60341a975371e5778f28dd477e6cf1147c.
    
    * commentを見るようにする
    
    * fix
    
    * fix
    
    * fix
    
    * Update scripts/action/update.sh
    
    Co-authored-by: Masaya Suzuki <15100604+massongit@users.noreply.github.com>
    
    * bug fix
    
    * vから始まる物をバージョンとする
    
    ---------
    
    Co-authored-by: Masaya Suzuki <15100604+massongit@users.noreply.github.com>

--- scripts/action/update.sh
+++ scripts/action/update.sh
@@ -3,12 +3,21 @@
 version=""
 
 while IFS= read -r -d '' f; do
-  v="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*")' "${f}" | sed -e 's/.*@//g')"
-  if [ -n "${v}" ]; then
-    version="${v}"
+  # versionがコメントに書かれている場合
+  version="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*") | line_comment' "${f}")"
+  if  [[ -z "${version}" || ! "${version}" =~ ^v.+ ]]; then
+    version="$(yq '.jobs.*.steps[].uses | select(. == "super-linter/super-linter*")' "${f}" | sed -e 's/.*@//g')"
+  fi
+  if [[ "${version}" =~ ^v.+ ]]; then
     break
   fi
 done < <(find .github/workflows -type f -print0)
+
+echo "super-linter version = ${version}"
+
+if [ -z "${version}" ]; then
+  exit 1
+fi
 
 curl "https://raw.githubusercontent.com/super-linter/super-linter/${version}/TEMPLATES/.gitleaks.toml" >.gitleaks.toml
 yq -i ".repos[].rev = \"$(docker run --entrypoint gitleaks "ghcr.io/super-linter/super-linter:slim-${version}" version | sed -e 's/\r//g')\"" .pre-commit-config.yaml
