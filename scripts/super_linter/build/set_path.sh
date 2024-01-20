4d348389cc2bcfd50d9f52c546999969d8491b32
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2024-01-20 21:39:58 +0900

    fix

--- scripts/super_linter/build/set_path.sh
+++ scripts/super_linter/build/set_path.sh
@@ -2,5 +2,5 @@
 
 npm ci
 action_version="$(yq '.jobs.build.steps[-1].uses | line_comment' .github/workflows/super-linter.yml)"
-PATH="$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter/slim-${action_version}" /bin/sh -c 'echo $PATH')"
+PATH="$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter:slim-${action_version}" /bin/sh -c 'echo $PATH')"
 echo "PATH=/github/workspace/node_modules/.bin:${PATH}" >> "$GITHUB_ENV"
